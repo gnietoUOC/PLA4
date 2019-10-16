b64abcaa5b0f89c3cd37be9732170e74f466cd95
Genar Nieto <gnieto@uoc.edu>
2019-10-16 16:34:58 +0200

    Versión mejorada (con comando de cambio de estado del LED)

--- SerialCmds2/SerialCmds2.ino
+++ SerialCmds2/SerialCmds2.ino
@@ -26,20 +26,20 @@ void loop() {
       Serial.println("Encendiendo");
       digitalWrite(LED_BUILTIN, HIGH);              // Encendemos el LED 
     } else {
-        if (!cmd.compareTo("OFF")) {
-          status = LOW;
-          Serial.println("Apagando");
-          digitalWrite(LED_BUILTIN, LOW);           // Apagamos el LED 
+      if (!cmd.compareTo("OFF")) {
+        status = LOW;
+        Serial.println("Apagando");
+        digitalWrite(LED_BUILTIN, LOW);           // Apagamos el LED 
+      } else {
+        if (!cmd.compareTo("CHG")) {
+          status = !status;
+          Serial.print("Cambiando ");
+          Serial.println(status? "(Encendiendo)":"(Apagando)");
+          digitalWrite(LED_BUILTIN, status);      // Cambiamos el estado del LED 
         } else {
-          if (!cmd.compareTo("CHG")) {
-            status = !status;
-            Serial.print("Cambiando ");
-            Serial.println(status? "(Encendiendo)":"(Apagando)");
-            digitalWrite(LED_BUILTIN, status);      // Cambiamos el estado del LED 
-          } else {
-            Serial.println("Comando desconocido");  // Comando desconocido
-          }
+          Serial.println("Comando desconocido");  // Comando desconocido
         }
+      }
     }
   }
 
