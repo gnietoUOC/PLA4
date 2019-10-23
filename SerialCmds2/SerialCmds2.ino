805aa2acdb530715286e5c613424810f448672ac
Genar Nieto <gnieto@uoc.edu>
2019-10-23 22:45:24 +0200

    Versión mejorada (con comando de cambio de estado del LED y con array de comandos)

--- SerialCmds2/SerialCmds2.ino
+++ SerialCmds2/SerialCmds2.ino
@@ -1,3 +1,8 @@
+const String PROGMEM cmd1 = "ON";
+const String PROGMEM cmd2 = "OFF";
+const String PROGMEM cmd3 = "CHG";
+const String cmds[] PROGMEM = {cmd1,cmd2,cmd3};
+
 int status = LOW;                     // Mantengo el status para poder hacer el cambio
 
 void setup() {
@@ -16,31 +21,52 @@ void loop() {
   
     String cmd= Serial.readString();  // Leemos el comando que nos llega del puerto serie
 
-    cmd.trim();                       // Eliminamos el CR que incluye al final
-    
     Serial.print("Comando: ");
     Serial.println(cmd);
-    
-    if (!cmd.compareTo("ON")) {
-      status = HIGH;
-      Serial.println("Encendiendo");
-      digitalWrite(LED_BUILTIN, HIGH);              // Encendemos el LED 
+
+    int n = checkCmd(cmd);
+    if (n<0) {
+      Serial.println("Comando desconocido");
     } else {
-      if (!cmd.compareTo("OFF")) {
-        status = LOW;
-        Serial.println("Apagando");
-        digitalWrite(LED_BUILTIN, LOW);           // Apagamos el LED 
-      } else {
-        if (!cmd.compareTo("CHG")) {
-          status = !status;
-          Serial.print("Cambiando ");
-          Serial.println(status? "(Encendiendo)":"(Apagando)");
-          digitalWrite(LED_BUILTIN, status);      // Cambiamos el estado del LED 
-        } else {
-          Serial.println("Comando desconocido");  // Comando desconocido
-        }
-      }
+      processCmd(n);
     }
   }
+}
 
+// Comprobamos si se ha introducido un comando válido
+int checkCmd(String cmd) {
+  int n = -1;
+  int i = 0;
+  
+  // Eliminamos el CR (si lo incluye) del final
+  cmd.trim();                       
+  
+  // Buscamos el ordinal del comando introducido
+  do {
+    if (!cmd.compareTo(cmds[i])) {
+      n = i;
+    }
+  } while (n<0 && ++i<sizeof(cmds));
+
+  return n;  
+}
+
+// Procesamos el comando correspondiente
+void processCmd(int n) {
+  switch (n) {
+    case 0:
+      Serial.println("Encendiendo");
+      digitalWrite(LED_BUILTIN, HIGH);    // Encendemos el LED del MKR1000
+      break;
+    case 1:
+      Serial.println("Apagando");
+      digitalWrite(LED_BUILTIN, LOW);     // Apagamos el LED del MKR1000
+      break;
+    case 2:
+      status = !status;
+      Serial.print("Cambiando ");
+      Serial.println(status? "(Encendiendo)":"(Apagando)");
+      digitalWrite(LED_BUILTIN, status);  // Cambiamos el estado del LED 
+      break;
+  }
 }
